b65c7701d830a9c28e30626781cbb65409735da5
Afzal Ahmad <113641536+afzalahmad1@users.noreply.github.com>
2023-05-07 13:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-//your JS code here. If required.
-const buttons = document.querySelectorAll('.btn');
-const stopButton = document.querySelector('.stop');
-const soundsFolder = './sounds/';
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const soundFile = soundsFolder + button.dataset.sound;
-    playSound(soundFile);
-  });
-});
-
-stopButton.addEventListener('click', () => {
-  stopSound();
-});
-
-let audio = null;
-
-function playSound(soundFile) {
-  if (audio) {
-    stopSound();
-  }
-  audio = new Audio(soundFile);
-  audio.play();
+const audio = document.getElementById("audio");
+function play(src) {
+	audio.src=src;
+	audio.play();
 }
 
-function stopSound() {
-  if (audio) {
-    audio.pause();
-    audio.currentTime = 0;
-    audio = null;
-  }
+function stop() {
+	audio.pause();
 }
